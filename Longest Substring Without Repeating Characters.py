13bb1cb812c1447387a710fe63d2c8c9b0fa1755
LawrenceKao <bightt53523@gmail.com>
2021-02-10 17:23:28 +0800

    modify Longest

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -14,3 +14,28 @@ class Solution:
 s = "abcabcbb"
 a = Solution()
 print(a.lengthOfLongestSubstring(s))
+
+#############################################################################################
+
+# 424. Longest Repeating Character Replacement
+
+# Input:  s = "ABAB", k = 2
+
+    def characterReplacement(self, s: str, k: int) -> int:
+        Windowstart 0
+        char_freq = {}
+        longest = 0
+        maxrepeatchar = 0
+        for Windowend in range(len(s)):
+            if s[Windowend] not in char_freq:
+                char_freq[s[Windowend]] = 0
+
+            char_freq[s[Windowend]] += 1
+            maxrepeatchar = max(maxrepeatchar, char_freq[s[Windowend]])
+
+            if ( Windowend - Windowstart + 1) - maxrepeatchar > k : # 目前長度 - 最大重複 > 可變更數  Ex: ABBB 最大重複為 B:3 ， 4-3 > 1:（False) 則可接受 
+                char_freq[s[Windowstart]] -= 1
+                Windowstart += 1
+
+            longest = max(longest, ( Windowend - Windowstart + 1))  
+        return longest
--- Longest_Palindromic_Substring.py
+++ Longest_Palindromic_Substring.py
@@ -4,16 +4,31 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        P = ""
-        
+        # s = "babad"
+        result = ""
+        maxlen = 0
+
         for i in range(len(s)):
-            for j in range(len(s), i , -1):
-                if( len(P) >= j-i):
-                    break
-                elif(s[i:j] == s[i:j][::-1]):
-                    P = s[i:j]
-                    break
-        return P
+            
+            # odd case
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > maxlen :
+                    result = s[l : r+1]
+                    maxlen = r - l + 1
+                l -= 1
+                r += 1
+            
+            # even case
+            l, r = i, i + 1
+            while l >= 0 and r <len(s) and s[l] == s[r]:
+                if (r - l + 1) > maxlen:
+                    result = s[l : r + 1]
+                    maxlen = r - l +1
+                l -= 1
+                r += 1
+
+        return result
                     
         
         
